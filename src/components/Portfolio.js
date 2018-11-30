a8e0c6cccf9e73aecab0f50bfc0d691ef9a74bbc
Maxwell Rediker <maxwell.rediker@gmail.com>
2018-11-30 10:00:40 -0800

    reorganizing CSS to SMACSS architecture using mobile first principle

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import mongodb from "../assets/mongodb.png";
 import git from "../assets/git.png";
-// import github from '../assets/github.png';
 import heroku from "../assets/heroku.png";
 import jquery from "../assets/jquery.png";
 import webpack from "../assets/webpack.png";
@@ -10,8 +9,8 @@ import postgresql from "../assets/postgresql.png";
 export default class Portfolio extends Component {
   show = event => {
     // document.querySelector()
-    let target = event.target;
-    console.log("HOVER", target);
+    // let target = event.target;
+    // console.log("HOVER", target);
   };
   // hide = event => {
 
@@ -54,14 +53,14 @@ export default class Portfolio extends Component {
               </li>
             </ul>
           </div>
-          <div className="icons__list__container">
-            <ul className="icons__list">
-              <li className="icons__list__item">
+          <div className="contact__list__container">
+            <ul className="contact__list">
+              <li className="contact__list__item">
                 <a target="_blank" href="https://github.com/GitHubMaxwell">
                   <i className="fab fa-github fa-2x" />
                 </a>
               </li>
-              <li className="icons__list__item">
+              <li className="contact__list__item">
                 <a
                   target="_blank"
                   href="https://www.linkedin.com/in/maxwellrediker/"
@@ -69,63 +68,68 @@ export default class Portfolio extends Component {
                   <i className="fab fa-linkedin fa-2x" />
                 </a>
               </li>
-              <li className="icons__list__item">
+              <li className="contact__list__item">
                 <a href="mailto:maxwell.rediker@gmail.com">
                   <i className="far fa-envelope" />
                 </a>
               </li>
             </ul>
           </div>
-          <div className="aboutme__container">
+          <div className="about__container">
             <p>
-              lorem ipsum is a placeholder text commonly used to demonstrate the
-              visual form of a document without relying on meaningful content.
+              Fullstack JavaScript developer with a service and sales
+              background. Highly motivated to work on skilled teams to build and
+              support intuitive applications that solve hard business and client
+              facing issues.
             </p>
           </div>
-          <div className="tech__container">
+          <div className="tech-icons__container">
             <p>Tech:</p>
-            <ul className="tech__list">
-              <li onMouseOver={this.show} className="tech__list__item tooltip">
+            <ul className="tech-icons__list">
+              <li
+                onMouseOver={this.show}
+                className="tech-icons__list__item tooltip"
+              >
                 <i className="fab fa-js" />
                 <p className="tooltip__text">JavaScript</p>
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <i className="fab fa-node-js" />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <i className="fab fa-react" />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <img src={webpack} />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <i className="fab fa-html5" />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <i className="fab fa-css3-alt" />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <i className="fab fa-sass" />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <i className="fab fa-aws" />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <i className="fab fa-docker" />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <img src={git} />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <img src={heroku} />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <img src={postgresql} />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <img src={mongodb} />
               </li>
-              <li className="tech__list__item">
+              <li className="tech-icons__list__item">
                 <img src={jquery} />
               </li>
             </ul>
